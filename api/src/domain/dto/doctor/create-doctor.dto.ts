847d82ea55506a1bbb49c80ce4edfe52c37c27c4
Christian Herrera <christian.herrera.onaindi@gmail.com>
2024-09-05 20:11:49 -0400

    chore: add swagger documentation

--- api/src/domain/dto/doctor/create-doctor.dto.ts
+++ api/src/domain/dto/doctor/create-doctor.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Appointment, MedicalRecord, Role, Status } from '@prisma/client';
 import {
   IsDate,
@@ -10,42 +11,96 @@ import {
 } from 'class-validator';
 
 export class CreateDoctorDto {
+  @ApiProperty({
+    example: 'Jhon',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(2)
   firstName: string;
+
+  @ApiProperty({
+    example: 'Smith',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(2)
   lastName: string;
+
+  @ApiProperty({
+    example: 40,
+    required: true,
+  })
   @IsNotEmpty()
   @IsNumber()
   age: number;
+
+  @ApiProperty({
+    example: 'example@domain.com',
+    required: true,
+  })
   @IsNotEmpty()
   @IsEmail()
   @IsString()
   email: string;
+
+  @ApiProperty({
+    example: '1234578910',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   password: string;
+
+  @ApiProperty({
+    example: '1990-05-10',
+    required: true,
+  })
   @IsNotEmpty()
+  @IsDate()
   @IsISO8601()
   birthday: Date;
+
+  @ApiProperty({ example: 'Male', required: true })
   @IsNotEmpty()
   @IsString()
   gender: string;
+
+  @ApiProperty({ example: 'normal', required: true })
   @IsNotEmpty()
   @IsString()
   role: Role = 'normal';
+
+  @ApiProperty({
+    example: 'oftamologo',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   speciality: string;
+
+  @ApiProperty({
+    example: 'BS231SH',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   license: string;
+
+  @ApiProperty({
+    example: '+584261587489',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   phone: string;
+
+  @ApiProperty({
+    example: 'active',
+    required: false,
+  })
   @IsNotEmpty()
   @IsString()
   status?: Status = 'active';
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,19 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  const options = new DocumentBuilder()
+    .setTitle('OnlyHealth API')
+    .setDescription('OnlyHealth API')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .addServer('https://c20-52-t-node-react.onrender.com/', 'Staging')
+    .addTag('ONLYH')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(PORT);
   Logger.log(`Backend ready in port ${PORT}`);
 }
